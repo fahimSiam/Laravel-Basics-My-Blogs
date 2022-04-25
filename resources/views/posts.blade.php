629c4009d22be7e2ee15c02f030536fa43680a71
fahimSiam <siamfahim02@gmail.com>
2022-04-25 16:22:26 +0600

    25/4

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,37 +9,32 @@
 @endsection--}}
 
 <x-layout>
-    @include('_post-header')
+    @include ('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card />
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-        </div>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No Posts yet.</p>
+        @endif
     </main>
-{{--<x-slot name="content">
-    @foreach($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-                <br>
-            </h1>
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name }}</a>
-            </p>
-            <div>
-                {{$post->excerpt}}
-            </div>
-        </article>
-    @endforeach
-        <a href="/">Go Back</a>
-</x-slot>--}}
+    {{--<x-slot name="content">
+        @foreach($posts as $post)
+            <article>
+                <h1>
+                    <a href="/posts/{{$post->slug}}">
+                        {{$post->title}}
+                    </a>
+                    <br>
+                </h1>
+                <p>
+                    By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name }}</a>
+                </p>
+                <div>
+                    {{$post->excerpt}}
+                </div>
+            </article>
+        @endforeach
+            <a href="/">Go Back</a>
+    </x-slot>--}}
 </x-layout>
