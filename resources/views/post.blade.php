073c564491fba28fef3baeb7f7366f35ed5d5ef0
fahimSiam <siamfahim02@gmail.com>
2022-05-06 02:24:36 +0600

    chapter 34 done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -36,60 +36,16 @@
                     </a>
 
                     <div class="space-x-2">
-                        <a href="/categories/{{$post->category->slug}}"
-                           class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                           style="font-size: 10px">{{$post->category->name}}</a>
+                        <x-category-button :category="$post->category" />
                     </div>
                 </div>
 
                 <h1 class="font-bold text-3xl lg:text-4xl mb-10">
-                    This is a big title and it will look great on two or even three
-                    lines. Wooohoo!
+                    {{$post->title}}
                 </h1>
 
                 <div class="space-y-4 lg:text-lg leading-loose">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur.</p>
-
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                        laudantium,
-                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-                        vitae
-                        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-                        fugit.</p>
-
-                    <h2 class="font-bold text-lg">Sed quia consequuntur</h2>
-
-                    <p>Magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-                        dolorem
-                        ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
-                        tempora
-                        incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-
-                    <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-                        nisi ut
-                        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate
-                        velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-                        nulla
-                        pariatur?"</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur.</p>
-
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                        laudantium,
-                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-                        vitae
-                        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-                        fugit.</p>
+                    {{$post->body}}
                 </div>
             </div>
         </article>
